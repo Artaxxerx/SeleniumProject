a556233482b48677deaf01dc7a84c5e36e1790a1
Aleksandr Belov <intercross12@yandex.ru>
2024-11-06 00:56:12 +0300

    new commit

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 
@@ -22,11 +21,6 @@ public abstract class BaseTest {
         File file = new File("src/test/resources/chromedriver.exe");
         System.setProperty("Webdriver.chrome.driver", file.getAbsolutePath());
         driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Добавляем headless-режим
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        WebDriver driver = new ChromeDriver(options);
         driver.get("https://qa-scooter.praktikum-services.ru/");
     }
 
